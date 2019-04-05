8204c0b9033f9e7d7d1ab98c5d1d8baf947ddc88
ViggieSmalls <victormovileanu@gmail.com>
2019-04-05 23:50:39 +0200

    added multiple row insertion via jquery

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -22,3 +22,14 @@ $input_source.on("keyup", function () {
    clearTimeout(timerIdentifier);
    timerIdentifier = setTimeout($preview_source.html($input_source.val()), doneTypingInterval);
 });
+
+$('.js-addDestination').on("click", function () {
+    let city = $("#city");
+    let price = $("#price");
+    let color = $("#color");
+    const table_row = "<tr><td>" + city.val() + "</td><td>" + price.val() + "</td><td><span class='dot' style='background-color: " + color.val() + "'></span></td></tr>";
+    $(table_row).insertAfter(".table-header");
+    city.empty();
+    price.empty();
+    color.empty()
+});
