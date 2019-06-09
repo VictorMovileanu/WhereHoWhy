fb8c5f074b4d4d5e8e99ed2e221112268dd9ab73
ViggieSmalls <victormovileanu@gmail.com>
2019-06-09 16:17:53 +0200

    changed url paths (20m)

--- skyfly/urls.py
+++ skyfly/urls.py
@@ -4,8 +4,8 @@ from .views import IndexView, skyfly_request, csv_serve_view, iata_codes_serve_v
 app_name = 'skyfly'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('request/<str:request_uuid>/', skyfly_request, name='request'),
+    path('request/<uuid:request_uuid>/', skyfly_request, name='request'),
+    path('request/<uuid:request_uuid>/data/', csv_serve_view, name='request-csv-data'),
+    path('request/<uuid:request_uuid>/info/', skyfly_request_info, name='request-info'),
     path('data/iata-codes/', iata_codes_serve_view, name='iata-codes'),
-    path('data/<str:request_uuid>/', csv_serve_view, name='request-csv-data'),
-    path('info/<str:request_uuid>/', skyfly_request_info, name='request-info')
 ]
